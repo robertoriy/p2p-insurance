d2b76e4091888571520af0536080903e999e670c
robertoriy <holdenxhume@gmail.com>
2024-03-24 03:58:07 +0700

    add jpa repositories

---
+++ src/main/java/ru/nsu/insurance/p2p/dao/UserRepository.java
@@ -0,0 +1,11 @@
+package ru.nsu.insurance.p2p.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.nsu.insurance.p2p.dao.entity.UserProfile;
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserProfile, Long> {
+}
+
